924250082289102d830866e0e22121a64f8802b5
mohammad haj mahmoud <73450813+MohammadHajMahmoud@users.noreply.github.com>
2023-08-12 00:51:10 +0300

    WABALABA DUB DUB

---
+++ rick&MortyStarter/script.js
@@ -0,0 +1,30 @@
+async function fetchCharacters() {
+  try {
+    const response = await fetch(
+      "https://rickandmortyapi.com/api/character?status=alive"
+    );
+    const data = await response.json();
+    const characters = data.results.slice(0, 50);
+
+    const characterList = document.getElementById("characterList");
+    characters.forEach((character) => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("card");
+      //this is easier than creating each item and giving it a textContent then appending it :)
+      listItem.innerHTML = `
+                <h2>${character.name}</h2>
+                <img src="${character.image}" >
+                <p>Species: ${character.species}</p>
+                <p>Gender: ${character.gender}</p>
+                <p>Location: ${character.location.name}</p>
+            `;
+      characterList.appendChild(listItem);
+    });
+  } catch (error) {
+    console.error("Error Loading ze data ", error);
+    const errorDiv = document.createElement("div");
+    errorDiv.textContent = " error occurred while Loading ze data :( ";
+    document.body.appendChild(errorDiv);
+  }
+}
+fetchCharacters();
